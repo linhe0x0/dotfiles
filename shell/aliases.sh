c706e5b54eb50b181fbe54745a9a04f28e07ba30
Lin He <linhe0x0@gmail.com>
2025-07-29 17:04:03 +0800

    feat(shell): add `runs` alias to quickly show package.json scripts

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -19,6 +19,7 @@ alias unproxy='unset http_proxy;unset https_proxy; unset all_proxy;'
 alias pretty='prettier -w $(git diff --name-only --diff-filter=AM)'
 
 alias runx='pnpm run $(jq -r ".scripts|to_entries[]|.key" package.json | fzf --tmux center --border-label=" pnpm run " --style full --preview="jq -r '\''.scripts.\"{}\"'\'' package.json | bat -l bash")'
+alias runs='cat package.json | jq ".scripts"'
 
 # Alias for open_brew_package_home function
 # Shortcut to quickly open a brew package's homepage
