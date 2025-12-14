8b50a1fcd3d569806cd5b94e737b67ac9318a5f7
Lin He <linhe0x0@gmail.com>
2025-12-14 21:12:18 +0800

    fix(fish): make proxy aliases chain with '&&' instead of ';'

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -4,8 +4,8 @@
 alias vim='/usr/local/bin/nvim'
 
 # Quick start or close proxy
-alias proxy='set -gx http_proxy http://proxy.internal:12345; set -gx https_proxy $http_proxy; set -gx all_proxy $http_proxy; set -gx HTTP_PROXY $http_proxy; set -gx HTTPS_PROXY $https_proxy;'
-alias unproxy='set -e http_proxy; set -e https_proxy; set -e all_proxy; set -e HTTP_PROXY; set -e HTTPS_PROXY;'
+alias proxy='set -gx http_proxy http://proxy.internal:12345 && set -gx https_proxy $http_proxy && set -gx all_proxy $http_proxy && set -gx HTTP_PROXY $http_proxy && set -gx HTTPS_PROXY $https_proxy'
+alias unproxy='set -e http_proxy && set -e https_proxy && set -e all_proxy && set -e HTTP_PROXY && set -e HTTPS_PROXY'
 alias pon='proxy'
 alias poff='unproxy'
 
