0104056ceac941e4090214df424d34b84427b26f
Lin He <linhe0x0@gmail.com>
2026-03-03 22:19:59 +0800

    fix(nvim): use local icons instead of lazyvim.config

--- nvim/lua/plugins/ui/bufferline.lua
+++ nvim/lua/plugins/ui/bufferline.lua
@@ -29,7 +29,7 @@ return {
         diagnostics = 'nvim_lsp',
         always_show_bufferline = false,
         diagnostics_indicator = function(_, _, diag)
-          local icons = require('lazyvim.config').icons.diagnostics
+          local icons = require('constants.icons').diagnostics
           local ret = (diag.error and icons.Error .. diag.error .. ' ' or '')
             .. (diag.warning and icons.Warn .. diag.warning or '')
           return vim.trim(ret)
