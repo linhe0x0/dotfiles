536884cb96e7e58c9f4aab20a6610a9c945f3fae
Lin He <linhe0x0@gmail.com>
2026-03-03 22:07:45 +0800

    fix(nvim): dynamically configure linters based on available executables

--- nvim/lua/plugins/coding/linting.lua
+++ nvim/lua/plugins/coding/linting.lua
@@ -5,12 +5,60 @@ return {
     config = function()
       local lint = require('lint')
 
-      lint.linters_by_ft = {
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        javascriptreact = { 'eslint' },
-        typescriptreact = { 'eslint' },
-      }
+      local function has_exec(cmd)
+        return vim.fn.executable(cmd) == 1
+      end
+
+      local linters_by_ft = {}
+
+      if has_exec('eslint') then
+        linters_by_ft.javascript = { 'eslint' }
+        linters_by_ft.typescript = { 'eslint' }
+        linters_by_ft.javascriptreact = { 'eslint' }
+        linters_by_ft.typescriptreact = { 'eslint' }
+      end
+
+      if has_exec('pylint') then
+        linters_by_ft.python = { 'pylint' }
+      end
+
+      if has_exec('golangci-lint') then
+        linters_by_ft.go = { 'golangci_lint' }
+      end
+
+      if has_exec('cargo') then
+        linters_by_ft.rust = { 'clippy' }
+      end
+
+      if has_exec('shellcheck') then
+        linters_by_ft.sh = { 'shellcheck' }
+        linters_by_ft.bash = { 'shellcheck' }
+        linters_by_ft.zsh = { 'shellcheck' }
+      end
+
+      if has_exec('hadolint') then
+        linters_by_ft.dockerfile = { 'hadolint' }
+      end
+
+      if has_exec('jsonlint') then
+        linters_by_ft.json = { 'jsonlint' }
+      end
+
+      if has_exec('yamllint') then
+        linters_by_ft.yaml = { 'yamllint' }
+      end
+
+      if has_exec('markdownlint') then
+        linters_by_ft.markdown = { 'markdownlint' }
+      end
+
+      if has_exec('stylelint') then
+        linters_by_ft.css = { 'stylelint' }
+        linters_by_ft.scss = { 'stylelint' }
+        linters_by_ft.less = { 'stylelint' }
+      end
+
+      lint.linters_by_ft = linters_by_ft
 
       local lint_augroup = vim.api.nvim_create_augroup('nvim-lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
