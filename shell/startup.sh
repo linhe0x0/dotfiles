0f7417bee8294b1456c6a1bfda5a2f6dc7156e05
Lin He <linhe0x0@gmail.com>
2026-09-11 11:09:54 +0800

    refactor(*): rename tmux default session to main

--- shell/startup.sh
+++ shell/startup.sh
@@ -1,8 +1,8 @@
 # Automatically enter tmux session after shell startup.
 if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
   # Only attach if the default session has no attached clients.
-  attached=$(tmux display-message -p -t default '#{session_attached}' 2>/dev/null)
+  attached=$(tmux display-message -p -t main '#{session_attached}' 2>/dev/null)
   if [ "${attached:-0}" -eq 0 ]; then
-    tmux attach-session -t default || tmux new-session -s default
+    tmux new -A -s main
   fi
 fi
