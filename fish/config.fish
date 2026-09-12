8e3f17272f06812779a2d4a8ca0394a902502541
Lin He <linhe0x0@gmail.com>
2026-09-12 12:02:13 +0800

    fix(fish): use fish syntax for set in Homebrew mirror env vars

--- fish/config.fish
+++ fish/config.fish
@@ -18,10 +18,10 @@ fish_add_path $HOME/.go/bin
 fish_add_path $HOME/.cargo/bin
 
 # Proxy brew source with ustc.
-set -gx HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
-set -gx HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
-set -gx HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
-set -gx HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+set -gx HOMEBREW_BREW_GIT_REMOTE 'https://mirrors.ustc.edu.cn/brew.git'
+set -gx HOMEBREW_CORE_GIT_REMOTE 'https://mirrors.ustc.edu.cn/homebrew-core.git'
+set -gx HOMEBREW_API_DOMAIN 'https://mirrors.ustc.edu.cn/homebrew-bottles/api'
+set -gx HOMEBREW_BOTTLE_DOMAIN 'https://mirrors.ustc.edu.cn/homebrew-bottles'
 
 # Set theme for bat syntax highlighting.
 # Or just call bat with the --theme=DarkNeon option
